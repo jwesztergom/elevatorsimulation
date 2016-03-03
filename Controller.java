ed12f52fc542fe5ad2ece316dee4d842c8864b4a
jwesztergom <jwesztergom@student.cccnj.edu>
2016-03-03 18:19:55 -0500

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -11,15 +11,18 @@
  */
 public class Controller
 {
+    private int maxFloor;
+    private int tick;
+    
     private UpList upList;
     private DownList downList;
     private SinkList sinkList;
     private InCarList incarList;
-    private int maxFloor;
+    
     private PassengerSource source;
     private ECar car;
-    private int tick;
     private Clock myClock;
+    private PList plist;
     
     /**
      * Constructor for objects of class Controller.
@@ -35,6 +38,7 @@ public class Controller
     
         source = new PassengerSource(upList, downList, maxFloor, myClock);
         car = new ECar(upList, downList, sinkList, incarList, maxFloor);
+        plist = new PList(myClock);
         
         
         run(10);
